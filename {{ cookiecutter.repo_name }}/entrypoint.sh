48841ea2f010031aa0bf69ef484fa39dc64604f0
Philip Lombardi <plombardi@datawire.io>
2016-07-22 01:17:36 -0400

    Support for Quark dependencies. Use the Makefile to install the service on the container etc.

--- {{ cookiecutter.repo_name }}/entrypoint.sh
+++ {{ cookiecutter.repo_name }}/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+set +u
+source /opt/{{ cookiecutter.project_name }}/venv/bin/activate
+set -u
+
 /usr/sbin/nginx && /usr/bin/uwsgi --ini /opt/{{ cookiecutter.project_name }}/config/uwsgi.ini
