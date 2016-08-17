28165b363f04bb5d72992260d8968204f1bed470
Philip Lombardi <plombardi@datawire.io>
2016-08-17 11:03:46 -0400

    Update to not use venv

--- {{ cookiecutter.repo_name }}/entrypoint.sh
+++ {{ cookiecutter.repo_name }}/entrypoint.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-set +u
-source /opt/{{ cookiecutter.project_name }}/venv/bin/activate
-set -u
-
 /usr/sbin/nginx && uwsgi --ini /opt/{{ cookiecutter.project_name }}/config/uwsgi.ini
